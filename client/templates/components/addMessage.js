f67dc38cdd13453995f4a8eb26ce74d7dee80867
Alisson <alissonplus@msn.com>
2015-10-14 20:06:42 -0300

    loading
    - adicionado loading em varios eventos
    - adicionado o evento de remover o foco apos add uma mensagem

--- client/templates/components/addMessage.js
+++ client/templates/components/addMessage.js
@@ -15,6 +15,11 @@ Template.addMessage.events({
 
             MeteoricCamera.getPicture(cameraOptions, function (error, data) {
               Session.set("photo", data);
+                IonLoading.show({
+                  customTemplate: '<i class="spinner"><svg viewBox="0 0 64 64"><g><defs><linearGradient id="sGD" gradientUnits="userSpaceOnUse" x1="55" y1="46" x2="2" y2="46"><stop offset="0.1" class="stop1"></stop><stop offset="1" class="stop2"></stop></linearGradient></defs><g stroke-width="4" stroke-linecap="round" fill="none" transform="rotate(38.2755 32 32)"><path stroke="url(#sGD)" d="M4,32 c0,15,12,28,28,28c8,0,16-4,21-9"></path><path d="M60,32 C60,16,47.464,4,32,4S4,16,4,32"></path><animateTransform values="0,32,32;360,32,32" attributeName="transform" type="rotate" repeatCount="indefinite" dur="750ms"></animateTransform></g></g></svg></i>',
+                  duration: 500,
+                  backdrop: true
+                });
             });
           } else {
             console.log('Roda apenas no cordova');
@@ -37,6 +42,11 @@ Template.addMessage.events({
 
               MeteoricCamera.getPicture(cameraOptions, function (error, data) {
                 Session.set("photo", data);
+                IonLoading.show({
+                  customTemplate: '<i class="spinner"><svg viewBox="0 0 64 64"><g><defs><linearGradient id="sGD" gradientUnits="userSpaceOnUse" x1="55" y1="46" x2="2" y2="46"><stop offset="0.1" class="stop1"></stop><stop offset="1" class="stop2"></stop></linearGradient></defs><g stroke-width="4" stroke-linecap="round" fill="none" transform="rotate(38.2755 32 32)"><path stroke="url(#sGD)" d="M4,32 c0,15,12,28,28,28c8,0,16-4,21-9"></path><path d="M60,32 C60,16,47.464,4,32,4S4,16,4,32"></path><animateTransform values="0,32,32;360,32,32" attributeName="transform" type="rotate" repeatCount="indefinite" dur="750ms"></animateTransform></g></g></svg></i>',
+                  duration: 500,
+                  backdrop: true
+                });
               });
 
             } else {
@@ -60,6 +70,11 @@ Template.addMessage.events({
 
       MeteoricCamera.getPicture(cameraOptions, function (error, data) {
         Session.set("photo", data);
+        IonLoading.show({
+          customTemplate: '<i class="spinner"><svg viewBox="0 0 64 64"><g><defs><linearGradient id="sGD" gradientUnits="userSpaceOnUse" x1="55" y1="46" x2="2" y2="46"><stop offset="0.1" class="stop1"></stop><stop offset="1" class="stop2"></stop></linearGradient></defs><g stroke-width="4" stroke-linecap="round" fill="none" transform="rotate(38.2755 32 32)"><path stroke="url(#sGD)" d="M4,32 c0,15,12,28,28,28c8,0,16-4,21-9"></path><path d="M60,32 C60,16,47.464,4,32,4S4,16,4,32"></path><animateTransform values="0,32,32;360,32,32" attributeName="transform" type="rotate" repeatCount="indefinite" dur="750ms"></animateTransform></g></g></svg></i>',
+          duration: 500,
+          backdrop: true
+        });
       });
 
     } else {
@@ -80,6 +95,11 @@ Template.addMessage.events({
 
       MeteoricCamera.getPicture(cameraOptions, function (error, data) {
         Session.set("photo", data);
+        IonLoading.show({
+          customTemplate: '<i class="spinner"><svg viewBox="0 0 64 64"><g><defs><linearGradient id="sGD" gradientUnits="userSpaceOnUse" x1="55" y1="46" x2="2" y2="46"><stop offset="0.1" class="stop1"></stop><stop offset="1" class="stop2"></stop></linearGradient></defs><g stroke-width="4" stroke-linecap="round" fill="none" transform="rotate(38.2755 32 32)"><path stroke="url(#sGD)" d="M4,32 c0,15,12,28,28,28c8,0,16-4,21-9"></path><path d="M60,32 C60,16,47.464,4,32,4S4,16,4,32"></path><animateTransform values="0,32,32;360,32,32" attributeName="transform" type="rotate" repeatCount="indefinite" dur="750ms"></animateTransform></g></g></svg></i>',
+          duration: 500,
+          backdrop: true
+        });
       });
 
     } else {
@@ -108,15 +128,18 @@ Template.addMessage.events({
                     document.querySelector('#message').value, // texto
                     (Session.get("photo"))? Session.get("photo") : '', // imagem
                     '', // video
-                    1,
+                    1
                 ],
                 function(error, result){
                     if(!error){
-                        toastr.success(
-                            result,
-                            '',
-                            {"progressBar": true}
-                        );
+                          IonLoading.show({
+                            customTemplate: '<i class="spinner"><svg viewBox="0 0 64 64"><g><defs><linearGradient id="sGD" gradientUnits="userSpaceOnUse" x1="55" y1="46" x2="2" y2="46"><stop offset="0.1" class="stop1"></stop><stop offset="1" class="stop2"></stop></linearGradient></defs><g stroke-width="4" stroke-linecap="round" fill="none" transform="rotate(38.2755 32 32)"><path stroke="url(#sGD)" d="M4,32 c0,15,12,28,28,28c8,0,16-4,21-9"></path><path d="M60,32 C60,16,47.464,4,32,4S4,16,4,32"></path><animateTransform values="0,32,32;360,32,32" attributeName="transform" type="rotate" repeatCount="indefinite" dur="750ms"></animateTransform></g></g></svg></i>',
+                            duration: 1000,
+                            backdrop: true
+                          });
+
+                        // remove o foco
+                        document.querySelector('#message').blur();
 
                         //remove os dados dos campos do form para evitar a duplicidade do registro
                         document.querySelector('#message').value = '';
