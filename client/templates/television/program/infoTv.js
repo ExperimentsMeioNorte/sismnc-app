7de86b44594ed17bac9bc9afeb18db6965291c69
Allan Alexandre <alexandesigner@gmail.com>
2015-10-14 21:54:58 -0300

    Botão e TV

--- client/templates/television/program/infoTv.js
+++ client/templates/television/program/infoTv.js
@@ -1,14 +1,22 @@
+
 Template.infoTv.helpers({
  // mostra a televisao
  playTvValidate: function(hour_begin, hour_end){
-   return Meteor.playTv.playValidate(hour_begin, hour_end);
+   return Meteor.playTv.playValidate(hour_begin, hour_end),
+   document.querySelector('body').classList.add('playTV');
  },
 
  buttonValidate: function(){
-   return Meteor.playTv.buttonPlayTv();
+   return Meteor.playTv.buttonPlayTv(),
+   document.querySelector('body').classList.add('buttonGoTV');
  },
 
  buttonProgramId: function(){
    return Meteor.playTv.programId;
  }
 });
+
+Template.infoTv.destroyed = function () {
+  document.querySelector('body').classList.remove('playTV');
+  document.querySelector('body').classList.remove('buttonGoTV');
+};
