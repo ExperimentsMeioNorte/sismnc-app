094fd6f5a1841dfafa242c5093ad946cb960a571
Alisson <alissonplus@msn.com>
2015-10-17 13:23:00 -0300

    perfil
    - adicionado a automacao de mostrar o usuario logado

--- client/layout.js
+++ client/layout.js
@@ -1,5 +1,18 @@
 var handle = LaunchScreen.hold();
 
+Tracker.autorun(function() {
+    Meteor.remote.subscribe('program');
+    Meteor.remote.subscribe('category');
+    Meteor.remote.subscribe('content');
+    Meteor.remote.subscribe('answer');
+    Meteor.remote.subscribe('poll');
+    Meteor.remote.subscribe('polluser');
+    Meteor.remote.subscribe('vehicle');
+    Meteor.remote.subscribe('user');
+    Meteor.subscribe('users');
+});
+
+
 Template.layout.rendered = function(){
   FastClick.attach(document.body);
   IonSideMenu.snapper.settings({disable: 'none'});
@@ -14,4 +27,10 @@ Template.layout.events({
     Meteor.logout();
     Router.go('authentication');
   }
+});
+
+Template.layout.helpers({
+    user: function(){
+        return [User.findOne({_id:localStorage.getItem('Meteor.userId'), status:1})];
+    }
 });
