b38f6c2503e46be392dee91f77549bbae7fcc40d
Alisson <alissonplus@msn.com>
2015-10-19 19:29:54 -0300

    perfil
    - correção ao mostrar os dados do perfil

--- client/layout.js
+++ client/layout.js
@@ -30,8 +30,8 @@ Template.layout.events({
 
 Template.layout.helpers({
     user: function(){
-      if(localStorage.getItem('Meteor.userId')){
-        return [User.findOne({_id:localStorage.getItem('Meteor.userId'), status:1})];
+      if(Meteor.userId()){
+        return [User.findOne({_id:localStorage.getItem('Meteor.userServerId'), status:1})];
       }else{
         return '';
       }
