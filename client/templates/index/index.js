39bf4bea42bc873c6cb731ed51519c04ada3d74a
Allan Alexandre <alexandesigner@gmail.com>
2015-10-26 22:35:47 -0300

    Remoção de lixos na public

--- client/templates/index/index.js
+++ client/templates/index/index.js
@@ -1,6 +1,12 @@
 // Ao sair
 Template.index.rendered = function(){
   //Session.set('splashLoaded', true);
+  IonPopup.prompt({
+    title: 'Qual seu telefone?',
+    okText: 'Salvar',
+    inputType: 'text',
+    inputPlaceholder: 'Digite aqui'
+  });
 }
 
 Template.index.destroyed = function(){
