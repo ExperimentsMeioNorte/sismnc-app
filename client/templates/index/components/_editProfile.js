0c87a01342cfe62edcbc323e5630b3dad87eb175
Alisson <alissonplus@msn.com>
2015-11-08 21:43:02 -0300

    profile
    - re adicionado os arquivos

---
+++ client/templates/index/components/_editProfile.js
@@ -0,0 +1,36 @@
+Template._editProfile.events({
+  'touchstart .update-perfil' : function(form){
+    form.preventDefault();
+    var name = document.querySelector("#name").value;
+    var email = document.querySelector("#mail").value;
+    if(!name || !email){
+        console.log('necessario preencher os campos obrigatorios');
+    }else{
+      Meteor.remote.call('updatePerfil',
+            [
+                222,
+                name,
+                email,
+                localStorage.getItem('Meteor.userId')
+            ],
+            function(error, result){
+                if(!result){
+                    console.log('algo deu errado.');
+                }else{
+                    console.log('atualizado com sucesso.')
+                }
+            }
+        );
+      }
+  }
+});
+
+Template._editProfile.helpers({
+    user: function(){
+        if(localStorage.getItem('Meteor.userId')){
+            return [User.findOne({_id:localStorage.getItem('Meteor.userId'), status:1})];
+        }else{
+            return '';
+        }
+    }
+});
