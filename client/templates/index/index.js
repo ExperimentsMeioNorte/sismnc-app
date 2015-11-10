3862fb54032b47df41eb680fa4ff81a3c910e3e9
Alisson <alissonplus@msn.com>
2015-11-09 22:20:23 -0300

    phone
    - correção ao salvar o telefone

--- client/templates/index/index.js
+++ client/templates/index/index.js
@@ -1,33 +1,35 @@
 Template.index.helpers({
   phone: function(){
     var userPhone = [User.findOne({_id:localStorage.getItem('Meteor.userServerId'), status:1})];
-    if(userPhone[0] === undefined || userPhone[0] === null){
-      IonPopup.prompt({
-        title: 'Qual seu telefone?',
-        okText: 'Salvar',
-        cancelText: 'Cancelar',
-        inputType: 'text',
-        inputPlaceholder: 'Digite aqui',
-        onOk: function() {
-          var phone = document.querySelector('input[name=prompt]').value;
-          if(phone){
-            Meteor.remote.call('updatePerfilPhone',
-              [
-                  222,
-                  phone,
-                  localStorage.getItem('Meteor.userId')
-              ],
-              function(error, result){
-                  if(!result){
-                      console.log('algo deu errado.');
-                  }else{
-                      console.log('atualizado com sucesso.');
-                  }
-              }
-            );
+    if(userPhone[0] !== undefined){
+      if(userPhone[0].phone === undefined || userPhone[0].phone === null){
+        IonPopup.prompt({
+          title: 'Qual seu telefone?',
+          okText: 'Salvar',
+          cancelText: 'Cancelar',
+          inputType: 'text',
+          inputPlaceholder: 'Digite aqui',
+          onOk: function() {
+            var phone = document.querySelector('input[name=prompt]').value;
+            if(phone){
+              Meteor.remote.call('updatePerfilPhone',
+                [
+                    222,
+                    phone,
+                    localStorage.getItem('Meteor.userId')
+                ],
+                function(error, result){
+                    if(!result){
+                        console.log('algo deu errado.');
+                    }else{
+                        console.log('atualizado com sucesso.');
+                    }
+                }
+              );
+            }
           }
-        }
-      });
+        });
+      }
     }
   }
 });
