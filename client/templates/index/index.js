46301863d9aefe2a5bb8b09aabba80cbe25c112b
Alisson <alissonplus@msn.com>
2015-11-09 21:21:49 -0300

    phone
    - adicionado a regra para salvar o telefone
    - se usuario tiver telefone não mostra o modal

--- client/templates/index/index.js
+++ client/templates/index/index.js
@@ -1,16 +1,36 @@
-// Ao sair
-Template.index.rendered = function(){
-  IonPopup.prompt({
-    title: 'Qual seu telefone?',
-    okText: 'Salvar',
-    cancelText: 'Cancelar',
-    inputType: 'text',
-    inputPlaceholder: 'Digite aqui',
-    onOk: function() {
-      console.log('Salvo');
+Template.index.helpers({
+  phone: function(){
+    var userPhone = [User.findOne({_id:localStorage.getItem('Meteor.userServerId'), status:1})];
+    if(userPhone[0] === undefined || userPhone[0] === null){
+      IonPopup.prompt({
+        title: 'Qual seu telefone?',
+        okText: 'Salvar',
+        cancelText: 'Cancelar',
+        inputType: 'text',
+        inputPlaceholder: 'Digite aqui',
+        onOk: function() {
+          var phone = document.querySelector('input[name=prompt]').value;
+          if(phone){
+            Meteor.remote.call('updatePerfilPhone',
+              [
+                  222,
+                  phone,
+                  localStorage.getItem('Meteor.userId')
+              ],
+              function(error, result){
+                  if(!result){
+                      console.log('algo deu errado.');
+                  }else{
+                      console.log('atualizado com sucesso.');
+                  }
+              }
+            );
+          }
+        }
+      });
     }
-  });
-}
+  }
+});
 
 Template.index.destroyed = function(){
 
