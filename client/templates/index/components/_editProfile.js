a190cad5e598c66b95a0d9a30793d561f9c8f0e9
Alisson <alissonplus@msn.com>
2015-11-09 21:22:21 -0300

    Perfil
    - adicionado o campo phone para ser atualizado
    - adicionado para alterar a foto

--- client/templates/index/components/_editProfile.js
+++ client/templates/index/components/_editProfile.js
@@ -1,8 +1,96 @@
 Template._editProfile.events({
+    'touchstart .upload-image': function(event, template) {
+        IonPopup.show({
+          buttons: [{
+            text: '<i class="ion-paperclip"></i>',
+            type: 'button-positive',
+            onTap: function() {
+              if (Meteor.isCordova) {
+                var cameraOptions = {
+                  width: 640,
+                  height: 480,
+                  quality:80,
+                  sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+                };
+
+                MeteoricCamera.getPicture(cameraOptions, function (error, data) {
+                    if(data){
+                        Meteor.remote.call('updatePerfilAvatar',
+                            [
+                                222,
+                                data,
+                                localStorage.getItem('Meteor.userId')
+                            ],
+                            function(error, result){
+                                if(!result){
+                                    console.log('algo deu errado.');
+                                }else{
+                                    console.log('atualizado com sucesso.');
+                                    ionModal.close();
+                                }
+                            }
+                        );
+                    }else{
+                        console.log('Deu erro a tirar uma foto.');
+                    }
+                });
+              } else {
+                console.log('Roda apenas no cordova');
+              }
+              document.querySelector('body').classList.add('show-file-message');
+              IonPopup.close();
+            }
+          },
+          {
+            text: '<i class="ion-android-camera"></i>',
+            type: 'button-positive',
+            onTap: function() {
+              if (Meteor.isCordova) {
+                var cameraOptions = {
+                  width: 640,
+                  height: 480,
+                  quality:80
+                };
+
+                MeteoricCamera.getPicture(cameraOptions, function (error, data) {
+                    if(data){
+                        Meteor.remote.call('updatePerfilAvatar',
+                            [
+                                222,
+                                data,
+                                localStorage.getItem('Meteor.userId')
+                            ],
+                            function(error, result){
+                                if(!result){
+                                    console.log('algo deu errado.');
+                                }else{
+                                    console.log('atualizado com sucesso.');
+                                    ionModal.close();
+                                }
+                            }
+                        );
+                    }else{
+                        console.log('Deu erro a pegar uma foto.');
+                    }
+                });
+
+              } else {
+                console.log('Roda apenas no cordova');
+              }
+
+              document.querySelector('body').classList.add('show-file-message');
+              IonPopup.close();
+            }
+          }]
+        });
+    },
+
   'touchstart .update-perfil' : function(form){
     form.preventDefault();
     var name = document.querySelector("#name").value;
     var email = document.querySelector("#mail").value;
+    var phone = document.querySelector("#phone").value;
+    var phone = (phone)? phone : null;
     if(!name || !email){
         console.log('necessario preencher os campos obrigatorios');
     }else{
@@ -11,13 +99,15 @@ Template._editProfile.events({
                 222,
                 name,
                 email,
+                phone,
                 localStorage.getItem('Meteor.userId')
             ],
             function(error, result){
                 if(!result){
                     console.log('algo deu errado.');
                 }else{
-                    console.log('atualizado com sucesso.')
+                    console.log('atualizado com sucesso.');
+                    ionModal.close();
                 }
             }
         );
