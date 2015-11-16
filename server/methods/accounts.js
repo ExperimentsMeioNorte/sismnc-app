78fc321ef33c1004fead56dbcdb2c9a7b3fa5511
Allan Alexandre <alexandesigner@gmail.com>
2015-11-16 17:00:54 -0300

    Facebook ID

--- server/methods/accounts.js
+++ server/methods/accounts.js
@@ -16,35 +16,13 @@ Meteor.startup(function() {
     ServiceConfiguration.configurations.update(
       { service: "facebook" },
       { $set: {
-          appId: "638977909535835",
-          secret: "28012ad2d935da4bdbfe26b78c23f77a",
+          appId: "580730978674615",
+          secret: "17f294e3ae5aa7e088aa88fbe042dac6",
           loginStyle: "popup"
         }
       },
       { upsert: true }
     );
-
-    //  // Facebook
-    // ServiceConfiguration.configurations.remove({
-    //     service: "facebook"
-    // });
-    // ServiceConfiguration.configurations.insert({
-    //     service: "facebook",
-    //     appId: "638977909535835",
-    //     secret: "28012ad2d935da4bdbfe26b78c23f77a",
-    //     loginStyle: "redirect"
-    // });
-
-    // // Google
-    // ServiceConfiguration.configurations.remove({
-    //     service: "google"
-    // });
-    // ServiceConfiguration.configurations.insert({
-    //     service: "google",
-    //     clientId: "89878768792-op9bqlm96avdu4g32p2bpvrd9ga9e7ap.apps.googleusercontent.com",
-    //     secret: "wT1pGMsvLfuncOXp1YP4hR5L",
-    //     loginStyle: "redirect"
-    // });
 
 
 });
