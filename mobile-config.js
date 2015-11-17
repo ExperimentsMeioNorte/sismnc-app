6f97ed1241b0a65ad06ca71995044c94e212f540
Alisson <alissonplus@msn.com>
2015-11-17 18:53:41 -0300

    versao
    - atualizado a versão para 0.1.35

--- mobile-config.js
+++ mobile-config.js
@@ -2,7 +2,7 @@ App.info({
   id: 'br.com.app.sistemameionorte',
   name: 'Meio Norte',
   description: 'Aplicativo Interativo Sistema Meio Norte de Comunicação',
-  version: '0.1.34'
+  version: '0.1.35'
 });
 
 App.icons({
