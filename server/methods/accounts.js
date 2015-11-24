868d9218d9c48dd4fac76f6ea7d28b2a910a6abf
Allan Alexandre <alexandesigner@gmail.com>
2015-11-24 17:08:47 -0300

    Alteração na configuração do Google
    é necessário testar no mobile

--- server/methods/accounts.js
+++ server/methods/accounts.js
@@ -1,28 +1,47 @@
 // OAUTH ( REDES SOCIAIS );
 Meteor.startup(function() {
 
-    ServiceConfiguration.configurations.update(
-      { service: "google" },
-      { $set: {
-          clientId: "89878768792-op9bqlm96avdu4g32p2bpvrd9ga9e7ap.apps.googleusercontent.com",
-          secret: "wT1pGMsvLfuncOXp1YP4hR5L",
-          loginStyle: "popup"
-        }
-      },
-      { upsert: true }
-    );
+    // Facebook
+    ServiceConfiguration.configurations.remove({
+      service: "facebook"
+    });
+    ServiceConfiguration.configurations.insert({
+      service: "facebook",
+      appId: "580730978674615",
+      secret: "17f294e3ae5aa7e088aa88fbe042dac6"
+    });
+
+    // Google
+    ServiceConfiguration.configurations.remove({
+      service: "google"
+    });
+    ServiceConfiguration.configurations.insert({
+      service: "google",
+      clientId: "89878768792-op9bqlm96avdu4g32p2bpvrd9ga9e7ap.apps.googleusercontent.com",
+      secret: "wT1pGMsvLfuncOXp1YP4hR5L"
+    });
 
 
-    ServiceConfiguration.configurations.update(
-      { service: "facebook" },
-      { $set: {
-          appId: "580730978674615",
-          secret: "17f294e3ae5aa7e088aa88fbe042dac6",
-          loginStyle: "popup"
-        }
-      },
-      { upsert: true }
-    );
+    // ServiceConfiguration.configurations.update(
+    //   { service: "google" },
+    //   { $set: {
+    //       clientId: "89878768792-op9bqlm96avdu4g32p2bpvrd9ga9e7ap.apps.googleusercontent.com",
+    //       secret: "wT1pGMsvLfuncOXp1YP4hR5L",
+    //       loginStyle: "popup"
+    //     }
+    //   }
+    // );
+
+
+    // ServiceConfiguration.configurations.update(
+    //   { service: "facebook" },
+    //   { $set: {
+    //       appId: "580730978674615",
+    //       secret: "17f294e3ae5aa7e088aa88fbe042dac6",
+    //       loginStyle: "popup"
+    //     }
+    //   }
+    // );
 
 
 });
