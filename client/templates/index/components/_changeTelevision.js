6b1d53482f666479039d7d57c0b225597aa35846
Allan Alexandre <alexandesigner@gmail.com>
2015-11-24 22:34:35 -0300

    Alterações television

--- client/templates/index/components/_changeTelevision.js
+++ client/templates/index/components/_changeTelevision.js
@@ -11,17 +11,38 @@ Template._changeTelevision.events({
 });
 
 Template._changeTelevision.helpers({
-  television: function(){
-      return City.find(
-        { status: 1 }
-      ).map(
-        function(c) {
-          return {
-            _idTv: c._id,
-            name: c.name,
-            image: c.image
-          };
+  programs: function(){
+    var vehicleTelevision = Vehicle.find({
+        name: 'Rede Meio Norte'
+    }).map( function(v){
+        return {_id: v._id}
+    });
+    var cityId = City.find({
+        name: 'Piauí'
+    }).map( function(c){
+        return {
+           _idTv: c.city_id
         }
-      );
- }
+    });
+
+    if(vehicleTelevision[0] && cityId[0] !== undefined){
+      return Program.find({
+          vehicle_id: vehicleTelevision[0]._id,
+          city_id: cityId[0]._id,
+          status: 1
+      }).map( function(p) {
+          return {
+            _id: p._id,
+            image_avatar: p.image_avatar,
+            name: p.name
+          };
+      });
+    } else{
+      return '';
+    }
+
+    console.log(vehicleTelevision);
+    console.log(cityId);
+
+  }
 });
