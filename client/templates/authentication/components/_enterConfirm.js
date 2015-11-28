43bb0b16b6cb6af144bc9c3ad46eefbbc14e43d9
Alisson <alissonplus@msn.com>
2015-11-27 23:26:29 -0300

    enterRegister / enterConfirm
    - registro do usuario completo
    - envio do código para o email
    - modal de confirmacao do codigo
    - reenvio do codigo de confirmacao

--- client/templates/authentication/components/_enterConfirm.js
+++ client/templates/authentication/components/_enterConfirm.js
@@ -1,11 +1,12 @@
-Template._enterRegister.helpers({
+Template._enterConfirm.helpers({
     confirmDataEmail: function(){
-        return Session.get('getupConfirmUserData')[1];
+        return (Session.get('getupConfirmUserData'))? Session.get('getupConfirmUserData')[1] : null;
     }
 });
 
-Template._enterRegister.events({
-    'touchstart .button-submit-confirm, click .button-submit-confirm': function () {
+Template._enterConfirm.events({
+    'touchstart .confirm, click .confirm': function (event) {
+        event.preventDefault();
         IonLoading.show({
             customTemplate: '<i class="spinner spinner-spiral"><svg viewBox="0 0 64 64"><g><defs><linearGradient id="sGD" gradientUnits="userSpaceOnUse" x1="55" y1="46" x2="2" y2="46"><stop offset="0.1" class="stop1"></stop><stop offset="1" class="stop2"></stop></linearGradient></defs><g stroke-width="4" stroke-linecap="round" fill="none" transform="rotate(196.349 32 32)"><path stroke="url(#sGD)" d="M4,32 c0,15,12,28,28,28c8,0,16-4,21-9"></path><path d="M60,32 C60,16,47.464,4,32,4S4,16,4,32"></path><animateTransform values="0,32,32;360,32,32" attributeName="transform" type="rotate" repeatCount="indefinite" dur="750ms"></animateTransform></g></g></svg></i>',
             backdrop: true
@@ -26,7 +27,7 @@ Template._enterRegister.events({
                         Session.get('getupConfirmUserData')[2],
                         '/images/avatar.jpg',
                         Session.get('getupConfirmUserData')[3],
-                        CryptoJS.MD5(Session.get('getupConfirmUserData')[4] + password).toString(),
+                        CryptoJS.MD5(Session.get('getupConfirmUserData')[4] + Session.get('getupConfirmUserData')[3]).toString(),
                         null,
                         null,
                         null,
@@ -35,6 +36,7 @@ Template._enterRegister.events({
                     ],
                     function(error, result){
                         IonLoading.hide();
+                        IonModal.close();
                         if(result){
                             toastr.info(
                               result,
@@ -45,7 +47,7 @@ Template._enterRegister.events({
                                 "timeOut": 3000
                               }
                             );
-                            IonModal.close();
+
                         }else{
                             toastr.info(
                               "Ops, " + error,
@@ -83,5 +85,45 @@ Template._enterRegister.events({
               }
             );
         }
+    },
+
+    'touchstart .resetCode, click .resetCode': function(event){
+        event.preventDefault();
+        IonLoading.show({
+            customTemplate: '<i class="spinner spinner-spiral"><svg viewBox="0 0 64 64"><g><defs><linearGradient id="sGD" gradientUnits="userSpaceOnUse" x1="55" y1="46" x2="2" y2="46"><stop offset="0.1" class="stop1"></stop><stop offset="1" class="stop2"></stop></linearGradient></defs><g stroke-width="4" stroke-linecap="round" fill="none" transform="rotate(196.349 32 32)"><path stroke="url(#sGD)" d="M4,32 c0,15,12,28,28,28c8,0,16-4,21-9"></path><path d="M60,32 C60,16,47.464,4,32,4S4,16,4,32"></path><animateTransform values="0,32,32;360,32,32" attributeName="transform" type="rotate" repeatCount="indefinite" dur="750ms"></animateTransform></g></g></svg></i>',
+            backdrop: true
+        });
+
+        Session.set(
+            'getupConfirmUserData',
+            [
+                Random.secret([5]),
+                'O codigo para confirmação foi enviado para o email (' + Session.get('getupConfirmUserData')[3] + ') e ao preencher abaixo use da forma que foi gerada, pois o codigo usa case sensitive.',
+                Session.get('getupConfirmUserData')[2],
+                Session.get('getupConfirmUserData')[3],
+                Session.get('getupConfirmUserData')[4]
+            ]
+        );
+
+        Meteor.remote.call(
+            'sendEmail',
+            [
+                Session.get('getupConfirmUserData')[3],
+                Meteor.smtpServerUsername,
+                'Código de confirmação app Sistema Meio Norte',
+                'Este é o seu código para confirmar o registro no app Sistema Meio Norte: <b>' + Session.get('getupConfirmUserData')[0] + '</b>'
+            ]
+        );
+
+        IonLoading.hide();
+        toastr.info(
+          "Codigo reenviado com sucesso.",
+          '',
+          {
+            "positionClass": "toast-top-center",
+            "tapToDismiss": true,
+            "timeOut": 3000
+          }
+        );
     }
 });
