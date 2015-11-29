1be564f9dea2493a923ae1ef0f6d852b50b94c6b
Alisson <alissonplus@msn.com>
2015-11-28 23:26:52 -0300

    enterRegister
    - correcao do envio para o index adicionando o localstorage necessario

--- client/templates/authentication/components/_enterConfirmRegister.js
+++ client/templates/authentication/components/_enterConfirmRegister.js
@@ -5,7 +5,7 @@ Template._enterConfirmRegister.helpers({
 });
 
 Template._enterConfirmRegister.events({
-    'touchstart .confirmRegister': function (event) {
+    'click .confirmRegister': function (event) {
         event.preventDefault();
         IonLoading.show({
             customTemplate: '<i class="spinner spinner-spiral"><svg viewBox="0 0 64 64"><g><defs><linearGradient id="sGD" gradientUnits="userSpaceOnUse" x1="55" y1="46" x2="2" y2="46"><stop offset="0.1" class="stop1"></stop><stop offset="1" class="stop2"></stop></linearGradient></defs><g stroke-width="4" stroke-linecap="round" fill="none" transform="rotate(196.349 32 32)"><path stroke="url(#sGD)" d="M4,32 c0,15,12,28,28,28c8,0,16-4,21-9"></path><path d="M60,32 C60,16,47.464,4,32,4S4,16,4,32"></path><animateTransform values="0,32,32;360,32,32" attributeName="transform" type="rotate" repeatCount="indefinite" dur="750ms"></animateTransform></g></g></svg></i>'
@@ -34,11 +34,12 @@ Template._enterConfirmRegister.events({
                         1
                     ],
                     function(error, result){
+                        console.log(error, result);
                         IonLoading.hide();
                         IonModal.close();
-                        if(result){
+                        if(result[0]){
                             toastr.info(
-                              result,
+                              result[0],
                               '',
                               {
                                 "positionClass": "toast-top-center",
@@ -46,10 +47,10 @@ Template._enterConfirmRegister.events({
                                 "timeOut": 3000
                               }
                             );
-                            Router.go('index');
-                            IonModal.close();
-                            IonLoading.hide();
 
+                            localStorage.setItem('Meteor.userServerId', result[1]);
+                            localStorage.setItem('Meteor.userId', result[1]);
+                            Router.go('index');
                         }else{
                             toastr.info(
                               "Ops, " + error,
@@ -89,7 +90,7 @@ Template._enterConfirmRegister.events({
         }
     },
 
-    'touchstart .resetCodeRegister': function(event){
+    'click .resetCodeRegister': function(event){
         event.preventDefault();
         IonLoading.show({
             customTemplate: '<i class="spinner spinner-spiral"><svg viewBox="0 0 64 64"><g><defs><linearGradient id="sGD" gradientUnits="userSpaceOnUse" x1="55" y1="46" x2="2" y2="46"><stop offset="0.1" class="stop1"></stop><stop offset="1" class="stop2"></stop></linearGradient></defs><g stroke-width="4" stroke-linecap="round" fill="none" transform="rotate(196.349 32 32)"><path stroke="url(#sGD)" d="M4,32 c0,15,12,28,28,28c8,0,16-4,21-9"></path><path d="M60,32 C60,16,47.464,4,32,4S4,16,4,32"></path><animateTransform values="0,32,32;360,32,32" attributeName="transform" type="rotate" repeatCount="indefinite" dur="750ms"></animateTransform></g></g></svg></i>'
--- client/templates/authentication/components/_enterRegister.js
+++ client/templates/authentication/components/_enterRegister.js
@@ -10,23 +10,20 @@ Template._enterRegister.events({
             customTemplate: '<i class="spinner spinner-spiral"><svg viewBox="0 0 64 64"><g><defs><linearGradient id="sGD" gradientUnits="userSpaceOnUse" x1="55" y1="46" x2="2" y2="46"><stop offset="0.1" class="stop1"></stop><stop offset="1" class="stop2"></stop></linearGradient></defs><g stroke-width="4" stroke-linecap="round" fill="none" transform="rotate(196.349 32 32)"><path stroke="url(#sGD)" d="M4,32 c0,15,12,28,28,28c8,0,16-4,21-9"></path><path d="M60,32 C60,16,47.464,4,32,4S4,16,4,32"></path><animateTransform values="0,32,32;360,32,32" attributeName="transform" type="rotate" repeatCount="indefinite" dur="750ms"></animateTransform></g></g></svg></i>'
         });
 
-        //document.querySelector('body').classList.add('show-confirm-code');
-
         var name = document.querySelector("#accountPassword_registerName").value;
         var email = document.querySelector("#accountPassword_registerEmail").value;
         var password = document.querySelector("#accountPassword_registerPassword").value;
 
         // valida se todos os campos obrigatorios estao preenchidos
         if(name !== '' && email !== '' && password !== ''){
-            var userId = User.findOne({
-                name: name,
+            var user = User.findOne({
                 email: email,
                 facebook_id: null,
                 google_id: null
             });
 
             // verifica se o usuario já foi cadastrado
-            if(userId === undefined){
+            if(user === undefined){
                 Session.set(
                     'getupConfirmUserData',
                     [
