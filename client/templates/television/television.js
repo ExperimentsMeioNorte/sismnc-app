4dc15df430b159c74b53ed99c010c77b030aef9a
Allan Alexandre <alexandesigner@gmail.com>
2015-12-11 01:02:45 -0300

    Loadings e Feedbacks

--- client/templates/television/television.js
+++ client/templates/television/television.js
@@ -1,4 +1,7 @@
 // Ao sair
+Template.television.rendered = function(){
+  document.querySelector('body').classList.add('television-page');
+}
 Template.television.destroyed = function(){
 
   document.querySelector('body').classList.remove('television-page');
