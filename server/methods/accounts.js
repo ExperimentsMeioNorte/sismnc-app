891574104d2f8df50d968709fad89db357bb0f7a
Allan Alexandre <alexandesigner@gmail.com>
2015-12-15 08:48:12 -0300

    Accounts.js (organizando)

--- server/methods/accounts.js
+++ server/methods/accounts.js
@@ -1,23 +1,36 @@
 // OAUTH ( REDES SOCIAIS );
+
+var google = {
+  clientId: '89878768792-horg1beja8ai32j24bg6k532ce2bhi5p.apps.googleusercontent.com',
+  clientSecret: 'xFsGTQfT8s1Ij6oxhlIpmUiI'
+}
+
+var facebook = {
+  appId: '580730978674615',
+  appSecret: '17f294e3ae5aa7e088aa88fbe042dac6'
+}
+
 Meteor.startup(function() {
 
     // Google
-    ServiceConfiguration.configurations.remove({
+    Accounts.loginServiceConfiguration.remove({
       service: "google"
     });
-    ServiceConfiguration.configurations.insert({
+    Accounts.loginServiceConfiguration.insert({
       service: "google",
-      clientId: "89878768792-op9bqlm96avdu4g32p2bpvrd9ga9e7ap.apps.googleusercontent.com",
-      secret: "wT1pGMsvLfuncOXp1YP4hR5L"
+      clientId: google.clientId,
+      secret: google.clientSecret,
+      loginStyle: "redirect",
+      redirectUrl: "/"
     });
 
-    ServiceConfiguration.configurations.remove({
+    Accounts.loginServiceConfiguration.remove({
       service: "facebook"
     });
-    ServiceConfiguration.configurations.insert({
+    Accounts.loginServiceConfiguration.insert({
       service: "facebook",
-      appId: "580730978674615",
-      secret: "17f294e3ae5aa7e088aa88fbe042dac6",
+      appId: facebook.appId,
+      secret: facebook.appSecret,
       loginStyle: "redirect",
       redirectUrl: "/"
     });
