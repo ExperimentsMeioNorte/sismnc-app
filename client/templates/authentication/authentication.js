150fb4baa61764d08d5aa032ea601144968b5432
Juis <alissonplus@gmail.com>
2015-12-16 23:51:19 -0300

    Adaptacao para login do facebook usando cordova, etapa 01

--- client/templates/authentication/authentication.js
+++ client/templates/authentication/authentication.js
@@ -17,22 +17,102 @@ Template.authentication.destroyed = function(){
 Template.authentication.events({
     // executa o login da rede social facebook
     'tap .bg-facebook': function (evento, tmp) {
+      /*IonLoading.show({
+        customTemplate: '<i class="spinner spinner-spiral"><svg viewBox="0 0 64 64"><g stroke-width="4" stroke-linecap="round"><line y1="17" y2="29" transform="translate(32,32) rotate(180)"><animate attributeName="stroke-opacity" dur="750ms" values="1;.85;.7;.65;.55;.45;.35;.25;.15;.1;0;1" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(210)"><animate attributeName="stroke-opacity" dur="750ms" values="0;1;.85;.7;.65;.55;.45;.35;.25;.15;.1;0" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(240)"><animate attributeName="stroke-opacity" dur="750ms" values=".1;0;1;.85;.7;.65;.55;.45;.35;.25;.15;.1" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(270)"><animate attributeName="stroke-opacity" dur="750ms" values=".15;.1;0;1;.85;.7;.65;.55;.45;.35;.25;.15" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(300)"><animate attributeName="stroke-opacity" dur="750ms" values=".25;.15;.1;0;1;.85;.7;.65;.55;.45;.35;.25" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(330)"><animate attributeName="stroke-opacity" dur="750ms" values=".35;.25;.15;.1;0;1;.85;.7;.65;.55;.45;.35" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(0)"><animate attributeName="stroke-opacity" dur="750ms" values=".45;.35;.25;.15;.1;0;1;.85;.7;.65;.55;.45" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(30)"><animate attributeName="stroke-opacity" dur="750ms" values=".55;.45;.35;.25;.15;.1;0;1;.85;.7;.65;.55" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(60)"><animate attributeName="stroke-opacity" dur="750ms" values=".65;.55;.45;.35;.25;.15;.1;0;1;.85;.7;.65" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(90)"><animate attributeName="stroke-opacity" dur="750ms" values=".7;.65;.55;.45;.35;.25;.15;.1;0;1;.85;.7" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(120)"><animate attributeName="stroke-opacity" dur="750ms" values=".85;.7;.65;.55;.45;.35;.25;.15;.1;0;1;.85" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(150)"><animate attributeName="stroke-opacity" dur="750ms" values="1;.85;.7;.65;.55;.45;.35;.25;.15;.1;0;1" repeatCount="indefinite"></animate></line></g></svg></i>'
+      });*/
 
-        IonLoading.show({
-          customTemplate: '<i class="spinner spinner-spiral"><svg viewBox="0 0 64 64"><g stroke-width="4" stroke-linecap="round"><line y1="17" y2="29" transform="translate(32,32) rotate(180)"><animate attributeName="stroke-opacity" dur="750ms" values="1;.85;.7;.65;.55;.45;.35;.25;.15;.1;0;1" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(210)"><animate attributeName="stroke-opacity" dur="750ms" values="0;1;.85;.7;.65;.55;.45;.35;.25;.15;.1;0" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(240)"><animate attributeName="stroke-opacity" dur="750ms" values=".1;0;1;.85;.7;.65;.55;.45;.35;.25;.15;.1" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(270)"><animate attributeName="stroke-opacity" dur="750ms" values=".15;.1;0;1;.85;.7;.65;.55;.45;.35;.25;.15" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(300)"><animate attributeName="stroke-opacity" dur="750ms" values=".25;.15;.1;0;1;.85;.7;.65;.55;.45;.35;.25" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(330)"><animate attributeName="stroke-opacity" dur="750ms" values=".35;.25;.15;.1;0;1;.85;.7;.65;.55;.45;.35" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(0)"><animate attributeName="stroke-opacity" dur="750ms" values=".45;.35;.25;.15;.1;0;1;.85;.7;.65;.55;.45" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(30)"><animate attributeName="stroke-opacity" dur="750ms" values=".55;.45;.35;.25;.15;.1;0;1;.85;.7;.65;.55" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(60)"><animate attributeName="stroke-opacity" dur="750ms" values=".65;.55;.45;.35;.25;.15;.1;0;1;.85;.7;.65" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(90)"><animate attributeName="stroke-opacity" dur="750ms" values=".7;.65;.55;.45;.35;.25;.15;.1;0;1;.85;.7" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(120)"><animate attributeName="stroke-opacity" dur="750ms" values=".85;.7;.65;.55;.45;.35;.25;.15;.1;0;1;.85" repeatCount="indefinite"></animate></line><line y1="17" y2="29" transform="translate(32,32) rotate(150)"><animate attributeName="stroke-opacity" dur="750ms" values="1;.85;.7;.65;.55;.45;.35;.25;.15;.1;0;1" repeatCount="indefinite"></animate></line></g></svg></i>'
-        });
+      evento.preventDefault();
 
-        evento.preventDefault();
+      Session.set('getupRemoveLoading', false);
 
-        Session.set('getupRemoveLoading', false);
+      // acessa o methodo das configuracoes para efetuar o login de uma determinada rede social
+      Meteor.loginApp(evento);
 
-        // acessa o methodo das configuracoes para efetuar o login de uma determinada rede social
-        Meteor.loginApp(evento);
+      // atributos montados a partir do methodo loginApp, como as opcoes e qual servidor de login é para executar
+      Meteor.loginAppService(Meteor.loginAppOptions, function(){
+        if(err){
+          //IonLoading.hide();
+          toastr.info(
+            "Estranho, mas ocorreu um problema.. tente novamente mais tarde",
+            '',
+            {
+              "positionClass": "toast-top-center",
+              "tapToDismiss": true,
+              "timeOut": 3000
+            }
+          );
+        }else{
+          var usersSearch = Meteor.users.findOne({_id:Meteor.userId()});
+          var userId = User.findOne(
+              {
+                facebook_id:usersSearch.services.facebook.id,
+                email:usersSearch.services.facebook.email
+              }
+          );
 
-        // atributos montados a partir do methodo loginApp, como as opcoes e qual servidor de login é para executar
-        Meteor.loginAppService(Meteor.loginAppOptions);
+          if(userId !== undefined){
+            if(userId.status === 0){
+              //IonLoading.hide();
+              toastr.info(
+                "Você não tem autorização, precisa de um login",
+                '',
+                {
+                  "positionClass": "toast-top-center",
+                  "tapToDismiss": true,
+                  "timeOut": 3000
+                }
+              );
+            }else{
+              localStorage.setItem('Meteor.facebookId', usersSearch.services.facebook.id);
+              localStorage.setItem('Meteor.emailId', usersSearch.services.facebook.email);
+              localStorage.setItem('Meteor.userServerId', userId._id);
+              localStorage.setItem('Meteor.userId', userId._id);
 
+              //IonLoading.hide();
+              Router.go('index');
+            }
+          }else{
+            Meteor.remote.call(
+                'insertUser',
+                [
+                  111,
+                  '0',
+                  usersSearch.services.facebook.name,
+                  'http://graph.facebook.com/' + usersSearch.services.facebook.id + '/picture/?type=small',
+                  usersSearch.services.facebook.email,
+                  null,
+                  usersSearch.services.facebook.id,
+                  null,
+                  null,
+                  1,
+                  1
+                ],
+                function(error, result){
+                  if(result){
+                    localStorage.setItem('Meteor.facebookId', usersSearch.services.facebook.id);
+                    localStorage.setItem('Meteor.emailId', usersSearch.services.facebook.email);
+                    localStorage.setItem('Meteor.userServerId', result[1]);
+                    localStorage.setItem('Meteor.userId', result[1]);
 
+                    //IonLoading.hide();
+                    Router.go('index');
+                  }else{
+                    //IonLoading.hide();
+                    toastr.info(
+                      "Estranho, " + error,
+                      '',
+                      {
+                        "positionClass": "toast-top-center",
+                        "tapToDismiss": true,
+                        "timeOut": 3000
+                      }
+                    );
+                  }
+                }
+            );
+          }
+        }
+      });
     },
 
     // login da rede social google
--- mobile-config.js
+++ mobile-config.js
@@ -2,7 +2,7 @@ App.info({
   id: 'br.com.app.sistemameionorte',
   name: 'Meio Norte',
   description: 'Aplicativo Interativo Sistema Meio Norte de Comunicação',
-  version: '0.1.53',
+  version: '0.1.54',
   author: 'Agência Getup!',
   email: 'alexandesigner@gmail.com',
   website: 'http://app.sistemameionorte.com.br'
@@ -11,9 +11,9 @@ App.info({
 App.icons({
 
   // iOS
-  'iphone': 'public/icons/ios/Icon-60.png',
+  /*'iphone': 'public/icons/ios/Icon-60.png',
   'iphone_2x': 'public/icons/ios/AppIcon.appiconset/Icon-60@2x.png',
-  'iphone_3x': 'public/icons/ios/AppIcon.appiconset/Icon-60@3x.png',
+  'iphone_3x': 'public/icons/ios/AppIcon.appiconset/Icon-60@3x.png',*/
 
   // Android
   'android_ldpi': 'public/icons/android/drawable-ldpi/ic_launcher.png',
@@ -26,11 +26,11 @@ App.icons({
 App.launchScreens({
 
   // iOS
-  'iphone': 'public/splash/ios/splash-320x480.png',
+  /*'iphone': 'public/splash/ios/splash-320x480.png',
   'iphone_2x': 'public/splash/ios/splash-320x480.png',
   'iphone5': 'public/splash/ios/splash-320x568.png',
   'iphone6': 'public/splash/ios/splash-375x667.png',
-  'iphone6p_portrait': 'public/splash/ios/splash-414x736.png',
+  'iphone6p_portrait': 'public/splash/ios/splash-414x736.png',*/
 
   // Android
   'android_ldpi_portrait': 'public/splash/android/drawable-ldpi/screen.png',
