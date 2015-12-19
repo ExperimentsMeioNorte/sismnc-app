d80b6222b27b34bcea8f756a184bfaa71836be81
Allan Alexandre <alexandesigner@gmail.com>
2015-12-18 23:50:49 -0300

    Version 0.10.57 / Cordova Facebook (tarball)

--- mobile-config.js
+++ mobile-config.js
@@ -2,7 +2,7 @@ App.info({
   id: 'br.com.app.sistemameionorte',
   name: 'Meio Norte',
   description: 'Aplicativo Interativo Sistema Meio Norte de Comunicação',
-  version: '0.1.56',
+  version: '0.1.57',
   author: 'Agência Getup',
   email: 'alissonplus@gmail.com',
   website: 'http://sistemameionorte.com.br'
