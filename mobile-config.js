dcf30ab8b63ec1e6b33bf7d11c199897f3634e08
Allan Alexandre <alexandesigner@gmail.com>
2015-12-21 09:24:46 -0300

    Version 0.10.57

--- mobile-config.js
+++ mobile-config.js
@@ -2,7 +2,7 @@ App.info({
   id: 'br.com.app.sistemameionorte',
   name: 'Meio Norte',
   description: 'Aplicativo Interativo Sistema Meio Norte de Comunicação',
-  version: '0.1.57',
+  version: '0.1.58',
   author: 'Agência Getup',
   email: 'alissonplus@gmail.com',
   website: 'http://sistemameionorte.com.br'
