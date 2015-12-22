56ac3be63113a63abf6e4223a6923f5719e1a049
Allan Alexandre <alexandesigner@gmail.com>
2015-12-22 00:31:11 -0300

    Loadings

--- mobile-config.js
+++ mobile-config.js
@@ -1,11 +1,3 @@
-// Regras de acesso cross origen
-App.accessRule("http://meteor.local");
-App.accessRule('http://sistemameionorte.com.br');
-App.accessRule('http://*.sistemameionorte.com.br');
-App.accessRule('http://meionorte.com');
-App.accessRule('http://*.meionorte.com');
-App.accessRule('*.google-analytics.com/*');
-
 App.info({
   id: 'br.com.app.sistemameionorte',
   name: 'Meio Norte',
@@ -60,8 +52,19 @@ App.setPreference('Orientation', 'portrait');
 App.setPreference("SplashScreen", "screen");
 App.setPreference("SplashScreenDelay", "1000");
 
+
 // cordova plugin facebook
 App.configurePlugin('com.phonegap.plugins.facebookconnect', {
      APP_ID: '580730978674615',
      APP_NAME: 'sistemamn'
 });
+
+// regras de acesso cross origen
+App.accessRule("*", {launchExternal: true});
+App.accessRule('*.google-analytics.com/*');
+App.accessRule("*://meteor.local");
+App.accessRule("*://*.meteor.com");
+App.accessRule("http://admin.sistemameionorte.com.br");
+App.accessRule("*://admin.sistemameionorte.com.br/*");
+App.accessRule("http://localhost:3000");
+App.accessRule("*://localhost:3000/*");
