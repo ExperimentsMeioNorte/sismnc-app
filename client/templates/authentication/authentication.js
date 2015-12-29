c98c427a3f3c87ad2afa3570769199eaf08d40b7
Juis <alissonplus@gmail.com>
2015-12-29 19:02:41 -0300

    Teste facebook connect 13

--- client/templates/authentication/authentication.js
+++ client/templates/authentication/authentication.js
@@ -1,6 +1,8 @@
 Template.authentication.rendered = function(){
   $('.pane').css('transform', 'translate3d(0, 0, 0)');
   document.querySelector('body').classList.remove('snapjs-left');
+  
+  navigator.app.exitApp();
 
   Meteor.setTimeout(function () {
     document.querySelector('.auth-box').classList.remove('auth-hide');
