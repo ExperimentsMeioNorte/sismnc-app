154d75771619750d10138bf4df2047a8bd9f09db
Juis <alissonplus@gmail.com>
2015-12-29 22:42:41 -0300

    Teste facebook connect 17

---
+++ server/methods/bugFacebook.js
@@ -0,0 +1,15 @@
+Meteor.methods({
+  	// insere a marca que foi configurado o primeiro uso ou atualizacao
+	'insertBugFacebook': function(){
+		BugFacebook.insert({validate: true});
+	},
+
+	// remove se tiver uma nova atualizacao
+	'removeBugFacebook': function(){
+	    BugFacebook.remove({});
+	}
+)};
+
+Meteor.publish('bugfacebook', function() {
+    return BugFacebook.find({});
+});
