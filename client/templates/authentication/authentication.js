d3e53074fdeb4a59c10a522477334baa93c18522
Juis <alissonplus@gmail.com>
2015-12-29 21:46:01 -0300

    Teste facebook connect 16

--- client/templates/authentication/authentication.js
+++ client/templates/authentication/authentication.js
@@ -1,6 +1,11 @@
 Template.authentication.rendered = function(){
   $('.pane').css('transform', 'translate3d(0, 0, 0)');
   document.querySelector('body').classList.remove('snapjs-left');
+  
+  alert(localStorage.getItem('Meteor.loginFacebook'));
+  if(!localStorage.getItem('Meteor.loginFacebook')){
+    localStorage.setItem('Meteor.loginFacebook', true);
+  }
 
   Meteor.setTimeout(function () {
     document.querySelector('.auth-box').classList.remove('auth-hide');
